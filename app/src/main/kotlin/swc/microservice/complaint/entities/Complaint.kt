178b814ba7ecce333132d168378af79c3d7cb2f8
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-07-04 21:13:19 +0200

    chore: add status to complaint

--- app/src/main/kotlin/swc/microservice/complaint/entities/Complaint.kt
+++ app/src/main/kotlin/swc/microservice/complaint/entities/Complaint.kt
@@ -1,3 +1,8 @@
 package swc.microservice.complaint.entities
 
-data class Complaint(val ownerId: String, val issuer: Issuer, val issue: String)
+data class Complaint(
+    val ownerId: String,
+    val issuer: Issuer,
+    val issue: String,
+    val status: ComplaintStatus = ComplaintStatus.OPEN
+)
