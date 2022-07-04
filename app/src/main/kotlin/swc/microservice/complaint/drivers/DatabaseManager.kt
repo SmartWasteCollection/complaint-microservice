82c97ee1917876b4ff56c4279eadc4a13f74979b
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-07-04 22:11:53 +0200

    chore: add database driver

---
+++ app/src/main/kotlin/swc/microservice/complaint/drivers/DatabaseManager.kt
@@ -0,0 +1,47 @@
+package swc.microservice.complaint.drivers
+
+import com.mongodb.MongoClient
+import com.mongodb.client.MongoCollection
+import org.bson.BsonDocument
+import org.bson.BsonElement
+import org.bson.BsonString
+import swc.microservice.complaint.entities.Complaint
+import swc.microservice.complaint.entities.ComplaintStatus
+import swc.microservice.complaint.entities.Issuer
+import swc.microservice.complaint.usecases.ComplaintManager
+
+class DatabaseManager : ComplaintManager {
+    private val collection: MongoCollection<BsonDocument>
+
+    init {
+        val mongoClient = MongoClient()
+        val database = mongoClient.getDatabase("smart-waste-collection")
+        this.collection = database.getCollection("complaints", BsonDocument::class.java)
+    }
+
+    override fun getAllComplaints(): List<Complaint> {
+        TODO()
+    }
+
+    override fun createComplaint(complaint: Complaint) {
+        val obj: BsonDocument = BsonDocument(
+            listOf(
+                BsonElement("id", BsonString("0")),
+                BsonElement("ownerId", BsonString("owner")),
+                BsonElement("title", BsonString("myTitle")),
+                BsonElement("issuer", BsonString(Issuer.USER.toString())),
+                BsonElement("message", BsonString("myMessage")),
+                BsonElement("status", BsonString(ComplaintStatus.OPEN.toString()))
+            )
+        )
+        this.collection.insertOne(obj)
+    }
+
+    override fun deleteComplaint(complaintId: String): Nothing {
+        TODO("Not yet implemented")
+    }
+
+    override fun closeComplaint(complaintId: String): Nothing {
+        TODO("Not yet implemented")
+    }
+}
