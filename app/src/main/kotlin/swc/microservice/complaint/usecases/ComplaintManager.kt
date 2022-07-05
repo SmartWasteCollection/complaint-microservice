f2d042454b66bc1718fd159df816743927cc6607
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-07-05 14:58:12 +0200

    chore: add use cases

--- app/src/main/kotlin/swc/microservice/complaint/usecases/ComplaintManager.kt
+++ app/src/main/kotlin/swc/microservice/complaint/usecases/ComplaintManager.kt
@@ -2,22 +2,12 @@ package swc.microservice.complaint.usecases
 
 import swc.microservice.complaint.entities.Complaint
 
-fun getAllComplaints(manager: ComplaintManager): List<Complaint> = manager.getAllComplaints()
-
-fun createComplaint(complaint: Complaint, manager: ComplaintManager) {
-    manager.createComplaint(complaint)
-}
-
-fun deleteComplaint(complaintId: String): Nothing = TODO()
-
-fun closeComplaint(complaintId: String): Nothing = TODO()
-
 interface ComplaintManager {
     fun getAllComplaints(): List<Complaint>
 
-    fun createComplaint(complaint: Complaint)
+    fun createComplaint(complaint: Complaint): String
 
-    fun deleteComplaint(complaintId: String): Nothing
+    fun deleteComplaint(complaintId: String): Complaint
 
-    fun closeComplaint(complaintId: String): Nothing
+    fun closeComplaint(complaintId: String): Complaint
 }
